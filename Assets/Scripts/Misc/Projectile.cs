86e00a00c20207e3b0a8b042636a1c3193216782
MrMFWIC <Benstar02@hotmail.ca>
2022-08-11 15:12:44 -0300

    Added Projectile and Camera Functionality

--- Assets/Scripts/Misc/Projectile.cs
+++ Assets/Scripts/Misc/Projectile.cs
@@ -6,27 +6,21 @@ public class Projectile : MonoBehaviour
 {
     public float speed;
     public float lifetime;
-    public GameObject[] powerupArray;
-    // Start is called before the first frame update
+    
     void Start()
     {
-        powerupArray = GameObject.FindGameObjectsWithTag("PowerUp");
-
         if (lifetime <= 0)
+        { 
             lifetime = 2.0f;
+        } 
 
         GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
         Destroy(gameObject, lifetime);
-
-        foreach (GameObject powerup in powerupArray)
-        {
-            Physics2D.IgnoreCollision(GetComponent<Collider2D>(), powerup.GetComponent<Collider2D>());
-        }
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag != "Player" && collision.gameObject.tag != "PowerUp")
+        if (collision.gameObject.tag != "Player")
         {
             Destroy(this.gameObject);
         }
