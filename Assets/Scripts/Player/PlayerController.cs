1895b605fe9b4c549e05ace5d13c2f5c9772c9be
MrMFWIC <Benstar02@hotmail.ca>
2022-08-11 00:54:41 -0300

    Added Animations and Movement

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator anim;
+
+    public float speed = 5.0f;
+    public int jumpForce = 300;
+    public bool isGrounded;
+    public LayerMask isGroundlayer;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.02f;
+    public bool isJumpAttack;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+        if (speed <= 0)
+        {
+            speed = 5.0f;
+        }
+
+        if (jumpForce <= 0)
+        {
+            jumpForce = 525;
+        }
+
+        if (groundCheckRadius <= 0)
+        {
+            groundCheckRadius = 0.02f;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimatorClipInfo[] curPlayingClip = anim.GetCurrentAnimatorClipInfo(0);
+        float hInput = Input.GetAxisRaw("Horizontal");
+        float vInput = Input.GetAxisRaw("Vertical");
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundlayer);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+
+        if (isGrounded)
+        {
+            rb.gravityScale = 1;
+        }
+
+        anim.SetFloat("MoveValue", Mathf.Abs(hInput));
+        anim.SetBool("isGrounded", isGrounded);
+
+        if (hInput != 0)
+        {
+            sr.flipX = (hInput < 0);
+        }
+
+        if (curPlayingClip.Length > 0)
+        {
+            if (Input.GetButtonDown("Fire1") && curPlayingClip[0].clip.name != "Attack")
+                anim.SetTrigger("Attack");
+            else if (curPlayingClip[0].clip.name == "Attack")
+                rb.velocity = Vector2.zero;
+            else
+            {
+                Vector2 moveDirection = new Vector2(hInput * speed, rb.velocity.y);
+                rb.velocity = moveDirection;
+            }
+        }
+    }
+}
